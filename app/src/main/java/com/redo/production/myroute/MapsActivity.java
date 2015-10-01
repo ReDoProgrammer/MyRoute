d19b6e1cfab479e3af5e3103ab1376b5222eb6fc
ReDo <redoprogrammer@gmail.com>
2015-10-01 23:59:54 +0700

    cout distance but incorrect

--- app/src/main/java/com/redo/production/myroute/MapsActivity.java
+++ app/src/main/java/com/redo/production/myroute/MapsActivity.java
@@ -3,6 +3,7 @@ package com.redo.production.myroute;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,11 +15,17 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DecimalFormat;
+
+import static java.lang.Math.acos;
+import static java.lang.Math.toRadians;
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private LatLng startPoint=null,currentPoint;
     TextView txvDistance,txvSpeed,txvTimeLeft;
+    double distance=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +36,9 @@ public class MapsActivity extends FragmentActivity {
         txvTimeLeft =(TextView) findViewById(R.id.txvTimeLeft);
 
     }
+
+
+
 
     @Override
     protected void onResume() {
@@ -43,6 +53,21 @@ public class MapsActivity extends FragmentActivity {
         txvSpeed.setText("0asf");
         txvTimeLeft.setText("aba");
         txvDistance.setText("hoho");
+    }
+
+
+    public double getDistance(LatLng fP, LatLng lP)
+    {
+        double l1 = toRadians(fP.latitude);
+        double l2 = toRadians(lP.latitude);
+        double g1 = toRadians(fP.longitude);
+        double g2 = toRadians(lP.longitude);
+
+        double dist = acos(Math.sin(l1) * Math.sin(l2) +  Math.cos(l1) *  Math.cos(l2) *  Math.cos(g1 - g2));
+        if(dist < 0) {
+            dist = dist + Math.PI;
+        }
+        return Math.round(dist * 6378100);
     }
 
 
@@ -74,12 +99,22 @@ public class MapsActivity extends FragmentActivity {
                     mMap.addMarker(new MarkerOptions().position(currentPoint).title("Current point"));
 
 
+
+
                     //set camera
                     currentPoint = new LatLng(arg0.getLatitude(), arg0.getLongitude());
                     CameraPosition currentPos = new CameraPosition.Builder()
                             .target(currentPoint).zoom(17).bearing(90).tilt(30).build();
                     mMap.animateCamera(
                             CameraUpdateFactory.newCameraPosition(currentPos));
+
+
+
+                    //update distance
+
+                    distance=getDistance(startPoint,currentPoint);
+                    txvDistance.setText(Double.toString(distance));
+
 
                 }
             });
